a6fd25e570680b8252dd5ae679c1a8d5dee296e2
projectlast2 <anton.bahtiyov@yandex.ru>
2025-04-18 16:55:30 +0300

    board

--- tictactoe.py
+++ tictactoe.py
@@ -16,14 +16,25 @@ class TicTacToe(QWidget):
 
         self.grid = QGridLayout()
         self.buttons = [[QPushButton("")for _ in range(3)]for _ in range(3)]
+        self.board = [[None for _ in range(3)]for _ in range(3)]
+
 
         for i in range(3):
             for j in range(3):
                 self.buttons[i][j].setFixedSize(100, 100)
                 self.buttons[i][j].setStyleSheet("font-size: 40px")
+                self.buttons[i][j].clicked.connect(
+                    lambda _, x=i, y =j: self.round(x,y)
+                )
                 self.grid.addWidget(self.buttons[i][j], i, j)
 
         self.setLayout(self.grid)
+
+    def round(self, x , y):
+        print(x,y)
+        if self.board[x][y] is None:
+            self.board[x][y]= "X"
+        self.buttons[x][y].setText("X")
 
 
         btns = []
